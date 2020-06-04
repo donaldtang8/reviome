d52c67e358e55d1ca8cf6548b549de5cebae5d04
Donald Tang <turazinyc@gmail.com>
2020-06-04 17:32:26 -0400

    Added package scripts, error handling controller, reusable document handler controller

---
+++ controllers/errorController.js
@@ -0,0 +1,147 @@
+const AppError = require('./../utils/appError');
+
+/**
+ * @function  handleCastErrorDB
+ * @description Handles errors dealing with invalid values for the database
+ * @return  AppError object with new error message
+ **/
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+/**
+ * @function  handleDuplicateFieldsDb
+ * @description Handles errors dealing with duplicate values for the database
+ * @return  AppError object with new error message
+ **/
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+/**
+ * @function  handleValidationErrorDB
+ * @description Handles errors dealing with validation errors for the database
+ * @return  AppError object with new error message
+ **/
+const handleValidationErrorDB = (err) => {
+  // loop over error object to look at other errors
+  const errors = Object.values(err.errors).map((elem) => elem.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+/**
+ * @function  handleJWTError
+ * @description Handles errors dealing with invalid tokens
+ * @return  AppError object with new error message
+ **/
+const handleJWTError = () =>
+  new AppError('Invalid token. Please log in again!', 401);
+
+/**
+ * @function  handleJWTExpiredError
+ * @description Handles errors dealing with expired tokens
+ * @return  AppError object with new error message
+ **/
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please log in again', 401);
+
+/**
+ * @function  sendErrorDev
+ * @description Send error in developmental environment
+ **/
+const sendErrorDev = (err, req, res) => {
+  // check if error being caught in the global error middleware is an "operational" error
+  // operational error - trusted error that we understand
+  // non-operational error - error that originates from exterior npm packages
+
+  // if call came from API, we want to show all the error details
+  if (req.originalUrl.startsWith('/api')) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+  // if call came from website, do not show error stack
+  else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+    });
+  }
+};
+
+/**
+ * @function  sendErrorProd
+ * @description Send error in production environment
+ **/
+const sendErrorProd = (err, res) => {
+  // if call came from API
+  if (req.originalUrl.startsWith('/api')) {
+    // A. API - Operational error - trusted error, show error details
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    // B. API - Developmental error - unknown error, do not leak error details
+    return res.status(500).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+  // if call came from website
+  // A. Website - Operational error - trusted error, show error details
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // B. Website - Developmental error - unknown error, do not leak error details
+  else {
+    res.status(500).json({
+      status: 'error',
+      message:
+        'Something went wrong, please try again or contact our support team',
+    });
+  }
+};
+
+/**
+ * @function  Error middleware
+ * @description Handles all errors that we pass and show error details. Express will automatically recognize this
+ *              function as an error middleware because of the order of the params (err, req, res, next)
+ **/
+module.exports = (err, req, res, next) => {
+  // if there is no statusCode associated with the error, by default it will be set to 500
+  err.statusCode = err.statusCode || 500;
+  // if there is no status associated with the error, by default it will be set to 'error'
+  err.status = err.status || 'error';
+  // if we are in the production environment, we let our helper functions create a user friendly error message before showing it to the user
+  if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    // use sendErrorProd function for remaining functions that are not explicitly caught
+    sendErrorProd(error, res);
+  }
+  // otherwise, if we are in the developmental environment, we can just show the error details as is
+  else {
+    sendErrorDev(err, req, res);
+  }
+  next();
+};
---
+++ controllers/handlerFactory.js
@@ -0,0 +1,121 @@
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+const APIFeatures = require('./../utils/APIFeatures');
+
+/**
+ * @function  createOne
+ * @description Creates a new document of the provided schema
+ **/
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    req.body.doc = doc;
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+    if (req.body.notif) {
+      next();
+    }
+  });
+
+/**
+ * @function  getAll
+ * @description Read all documents of the provided schema
+ **/
+exports.getAll = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    // for searching reviews based on postId
+    let filter = {};
+    if (req.params.postId) filter = { post: req.params.postId };
+    let query = Model.find();
+    if (popOptions) query = query.populate(popOptions);
+
+    const features = new APIFeatures(query, req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        doc,
+      },
+    });
+  });
+
+/**
+ * @function  createOne
+ * @description Creates a new document of the provided schema
+ **/
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+
+/**
+ * @function  updateOne
+ * @description Update document of the provided schema
+ **/
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+
+/**
+ * @function  deleteOne
+ * @description Delete document of the provided schema
+ **/
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+
+    if (req.body.notif) {
+      next();
+    }
+  });
