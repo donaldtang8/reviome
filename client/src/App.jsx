f8e5b33b1c7931eadcfa7af1ab17e707f9ca2021
Donald Tang <turazinyc@gmail.com>
2020-06-05 03:57:31 -0400

    Added package dependencies, scripts, added basic container tags, router, and created redux store

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="body__container">
+          <div className="main__container">
+            <div className="content__container"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+App.propTypes = {};
+
+const mapStateToProps = (state) => ({});
+
+export default connect(mapStateToProps, {})(App);
---
+++ client/src/redux/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore } from "redux-persist";
+
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const middlewares = [thunk];
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+export const persistor = persistStore(store);
+
+export default { store, persistStore };
