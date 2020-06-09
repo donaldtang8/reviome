21ea59c21700896d08d238afc54067b42fe27c99
Donald Tang <turazinyc@gmail.com>
2020-06-09 00:40:19 -0400

    Added authentication action page components

---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { register } from './../../redux/actions/auth';
+
+const Register = ({ auth: { isAuthenticated }, register, history }) => {
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push('/');
+    }
+  }, []);
+
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const {
+    firstName,
+    lastName,
+    username,
+    email,
+    password,
+    passwordConfirm,
+  } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    register(formData, history);
+  };
+
+  return (
+    <div className="section__container auth__container">
+      <div className="auth__container--form">
+        <form className="form__container" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <input
+            type="password"
+            name="passwordConfirm"
+            placeholder="Password Confirm"
+            value={passwordConfirm}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+
+          <input type="submit" value="Register" />
+        </form>
+        <Link to="/login">Have an account already?</Link>
+      </div>
+    </div>
+  );
+};
+
+Register.propTypes = {
+  register: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { register })(withRouter(Register));
---
+++ client/src/pages/auth/reset-password.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+
+import { forgotPassword } from './../../redux/actions/auth';
+
+const ForgotPassword = ({
+  auth: { isAuthenticated },
+  forgotPassword,
+  history,
+}) => {
+  useEffect(() => {
+    if (isAuthenticated) {
+      history.push('/');
+    }
+  }, []);
+
+  const [formData, setFormData] = useState({
+    email: '',
+  });
+
+  const { email } = formData;
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    forgotPassword(formData, history);
+  };
+
+  return (
+    <div className="section__container auth__container">
+      <form className="auth__container--form" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      <Link to="/login">Back to login</Link>
+    </div>
+  );
+};
+
+ForgotPassword.propTypes = {
+  forgotPassword: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { forgotPassword })(
+  withRouter(ForgotPassword)
+);
