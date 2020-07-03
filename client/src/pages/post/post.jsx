bd796ff54d7ea8cd08efeddb32d741b30fcbc9ea
Donald Tang <turazinyc@gmail.com>
2020-07-03 17:37:38 -0400

    Added report form popup in single post view page

--- client/src/pages/post/post.jsx
+++ client/src/pages/post/post.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import PropTypes from 'prop-types';
@@ -7,11 +7,27 @@ import { getPostById } from './../../redux/actions/posts';
 import PostItem from './../../components/post/post-item';
 
 import Spinner from './../../components/spinner/spinner';
+import ReportForm from './../../components/report/report-form';
 
 const Post = ({ getPostById, posts: { post, loading }, match }) => {
   useEffect(() => {
     getPostById(match.params.id);
   }, [getPostById]);
+
+  // reportOpen will toggle report form
+  const [reportOpen, setReportOpen] = useState(false);
+  // item refers to the item object in ReportForm
+  const [reportItem, setReportItem] = useState(null);
+
+  // Callback to toggle report open property
+  const reportOpenCallback = (open) => {
+    setReportOpen(open);
+  };
+
+  // Callback to return post object from post dropdown component
+  const reportItemCallback = (itemData) => {
+    setReportItem(itemData);
+  };
 
   return loading ? (
     <Spinner />
@@ -20,7 +36,23 @@ const Post = ({ getPostById, posts: { post, loading }, match }) => {
       {post === null ? (
         <div>No post found.</div>
       ) : (
-        <PostItem key={post._id} post={post} />
+        <Fragment>
+          <PostItem
+            key={post._id}
+            post={post}
+            reportOpenCallback={reportOpenCallback}
+            reportItemCallback={reportItemCallback}
+          />
+          {reportItem && (
+            <ReportForm
+              item={reportItem}
+              type="Post"
+              reportOpen={reportOpen}
+              reportItemCallback={reportItemCallback}
+              reportOpenCallback={reportOpenCallback}
+            />
+          )}
+        </Fragment>
       )}
     </div>
   );
