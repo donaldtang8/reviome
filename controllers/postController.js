0f3b5a5b1bca9e9160d8bfdd98489078f8f4c7a7
Donald Tang <turazinyc@gmail.com>
2020-07-15 02:01:04 -0400

    Fixed import names in controllers

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const AppError = require('./../utils/appError');
-const APIFeatures = require('./../utils/APIFeatures');
+const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
