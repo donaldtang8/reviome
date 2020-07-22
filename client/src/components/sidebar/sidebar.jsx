15b28fea20d4be1742bb29f02e94992bbdc09d65
Donald Tang <turazinyc@gmail.com>
2020-07-22 15:45:26 -0400

    Added administrative pages in sidebar

--- client/src/components/sidebar/sidebar.jsx
+++ client/src/components/sidebar/sidebar.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-const Sidebar = () => {
+const Sidebar = ({ auth }) => {
   const [nav, setNav] = useState('feed');
 
   const handleClick = (e, item) => {
@@ -45,9 +47,43 @@ const Sidebar = () => {
             Favorites
           </div>
         </Link>
+        {auth.user.role === 'admin' && (
+          <Fragment>
+            <Link to="/reports" onClick={() => setNav('reports')}>
+              <div
+                className={
+                  nav === 'reports'
+                    ? 'sidebar__nav--item sidebar__nav--selected'
+                    : 'sidebar__nav--item'
+                }
+              >
+                Reports
+              </div>
+            </Link>
+            <Link to="/manage/categories" onClick={() => setNav('categories')}>
+              <div
+                className={
+                  nav === 'categories'
+                    ? 'sidebar__nav--item sidebar__nav--selected'
+                    : 'sidebar__nav--item'
+                }
+              >
+                Categories
+              </div>
+            </Link>
+          </Fragment>
+        )}
       </div>
     </div>
   );
 };
 
-export default Sidebar;
+Sidebar.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, {})(Sidebar);
