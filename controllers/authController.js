052e710563edd33f093474304bc1c3e3eba40a5b
Donald Tang <turazinyc@gmail.com>
2020-07-26 06:07:20 -0400

    Temporarily disabled welcome email on registration action

--- controllers/authController.js
+++ controllers/authController.js
@@ -142,8 +142,8 @@ exports.signup = catchAsync(async (req, res, next) => {
     pass: req.body.password,
     passConfirm: req.body.passwordConfirm,
   });
-  const URL = `${req.protocol}://${req.headers['x-forwarded-host']}/account`;
-  await new Email(newUser, URL).sendWelcome();
+  // const URL = `${req.protocol}://${req.headers['x-forwarded-host']}/account`;
+  // await new Email(newUser, URL).sendWelcome();
   createSendToken(newUser, 201, res);
 });
 
